2bc8ad12a078f5278df43c3ab1c97cd2f5c3a1f9
s <ssoehnlen@protonmail.com>
2025-10-15 18:17:16 +0200

    adding src

---
+++ Fundamentals_ARM_Assembly/armtest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  printf("Hey there, this is  ARM :)\n");
+  return 0; 
+}
+
+
+/* to compile:  arm-linux-gnueabihf-gcc -o test armtest.c -static */ 
+
+/* to run  ./test  OR  qemu-arm ./test                            */
